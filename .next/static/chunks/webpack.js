5909db2951ad9daef3cd20a11c67548f03590606
Kritidipto Ghosh <kritidipto.ghosh@wingify.com>
2023-05-28 21:51:18 +0530

    Fixing node version

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "69ef9607cdc4192163e0";
+/******/ 	var hotCurrentHash = "f72342bffbf3dc58d810";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
